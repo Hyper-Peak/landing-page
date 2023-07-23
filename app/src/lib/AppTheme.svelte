dc88eb0b6de23febe6c6456baf0101cf187d3763
iliyangermanov <iliyan.germanov971@gmail.com>
2023-07-24 00:08:02 +0300

    WIP: Styling

--- app/src/lib/AppTheme.svelte
+++ app/src/lib/AppTheme.svelte
@@ -1,18 +1,43 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import { colorsToCSS, ds } from '$lib/theme';
+	import HomeButton from './HomeButton.svelte';
 	import ThemeToggle from './ThemeToggle.svelte';
 </script>
 
 <div id="root" class="fullscreen" style={colorsToCSS($ds.colors)}>
-	<ThemeToggle />
-	<slot />
+	<div id="appbar">
+		{#if $page.url.pathname !== '/'}
+			<HomeButton />
+		{/if}
+		<div style="margin-left: auto;">
+			<ThemeToggle />
+		</div>
+	</div>
+	<div id="content">
+		<slot />
+	</div>
 </div>
 
 <style>
-	:global(#root) {
+	#root {
 		width: 100vw;
 		height: 100vh;
+		padding: 32px;
 		background-color: var(--bg);
+		box-sizing: border-box;
+	}
+
+	#appbar {
+		display: flex;
+		flex-direction: row;
+	}
+
+	#content {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+		align-items: center;
 	}
 
 	:global(h1, h2, h3, h4, p) {
