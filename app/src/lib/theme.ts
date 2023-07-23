c6ecccbc234e535a04cda690f85b96b383c3ab3b
iliyangermanov <iliyan.germanov971@gmail.com>
2023-07-23 21:38:08 +0300

    WIP: Light/Dark theme

--- app/src/lib/theme.ts
+++ app/src/lib/theme.ts
@@ -1,24 +1,48 @@
-// themeStore.js
-import { writable, get } from 'svelte/store';
 import { onMount } from 'svelte';
+import { derived, writable } from 'svelte/store';
 
-// create the store with an initial 'light' theme
-export const theme = writable('light');
+export const WHITE: string = '#f8f8ff'
+export const BLACK: string = '#1c1c1c'
+export const GRAY: string = '#c0c0c0'
 
-export function initializeTheme() {
-  let systemTheme;
-  onMount(() => {
-    // Now we're in the browser context, access window and localStorage
-    systemTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    const storedTheme = localStorage.getItem('theme') || systemTheme;
-    theme.set(storedTheme);
-  });
+export type DS = {
+  isLight: boolean
+  colors: Colors
 }
 
-export function switchTheme() {
-  theme.update(value => {
-    const newTheme = value === 'light' ? 'dark' : 'light';
-    localStorage.setItem('theme', newTheme);
-    return newTheme;
-  });
+export type Colors = {
+  bg: string
+  content: string
+  gray: string
 }
+
+const lightColors = {
+  bg: WHITE,
+  content: BLACK,
+  gray: GRAY,
+}
+
+const darkColors = {
+  bg: BLACK,
+  content: WHITE,
+  gray: GRAY,
+}
+
+export const ds = writable<DS>({
+  isLight: true,
+  colors: lightColors,
+})
+
+onMount(() => {
+  matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+    ds.update((state) => ({
+      isLight: state.isLight,
+      colors: state.isLight ? lightColors : darkColors,
+    }))
+  })
+})
+
+export const dsStyles = derived(ds, $ds => Object.entries($ds.colors)
+  .map(([key, value]) => `--${key}:${value}`)
+  .join(';')
+)
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,50 +1,21 @@
-<script>
-	import { initializeTheme, theme } from '$lib/theme';
-	import '../app.css';
-
-	const white = '#f8f8ff';
-	const black = '1c1c1c';
-
-	let styles = {
-		pure: white,
-		pureInv: black
-	};
-
-	$: cssVarStyles = Object.entries(styles)
-		.map(([key, value]) => `--${key}:${value}`)
-		.join(';');
-
-	initializeTheme();
-
-	// Set the theme attribute on the HTML element whenever the store value changes
-	theme.subscribe((value) => {
-		if (value === 'light') {
-			styles = {
-				pure: white,
-				pureInv: black
-			};
-		} else {
-			styles = {
-				pure: black,
-				pureInv: white
-			};
-		}
-	});
+<script lang="ts">
+	import { dsStyles, ds } from '$lib/theme';
 </script>
 
+<div id="bg" style={$dsStyles}>
+	<slot />
+</div>
+
 <style>
-	body {
-		background-color: var(--pure, #f8f8ff);
+	#bg {
+		background-color: var(--bg);
 	}
 
 	h1,
 	h2,
 	h3,
+	h4,
 	p {
-		color: var(--pureInv, #1c1c1c);
+		color: var(--content);
 	}
 </style>
-
-<div style={cssVarStyles}>
-	<slot />
-</div>
