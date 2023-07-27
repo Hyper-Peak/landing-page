45a1702a7097bcd4c9a8b0f36681c0abeccde73e
iliyangermanov <iliyan.germanov971@gmail.com>
2023-07-27 18:03:15 +0300

    Style the home page

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div id="main">
-	<h1 id="title" class="xxl mt48"><b><i>HyperPeak</i></b></h1>
+	<h1 id="title" class="xxl"><b><i>HyperPeak</i></b></h1>
 
 	<p class="l mt32">A software service company.</p>
 	<p class="l mt16">
@@ -18,7 +18,7 @@
 
 <div id="tldr">
 	<h3 id="how">How does it work?</h3>
-	<ol class="mt20">
+	<ol>
 		<li>Tell us your project and budget. 💡</li>
 		<li>We'll confirm if we can build it. 📊</li>
 		<li>Together we clarify the details and get started. 🔨</li>
@@ -39,6 +39,7 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+		justify-content: center;
 		height: 40%;
 		padding-bottom: 24px;
 	}
@@ -46,10 +47,6 @@
 	#title {
 		line-height: var(--xxl);
 		padding: 0px;
-	}
-
-	#how {
-		font-size: var(--xl);
 	}
 
 	.divider {
@@ -63,9 +60,8 @@
 
 	#tldr {
 		display: flex;
-		padding-top: 24px;
-
-		padding-bottom: 48px;
+		padding-top: 32px;
+		padding-bottom: 64px;
 		flex-direction: column;
 		align-items: center;
 		border-radius: 32px 32px 0px 0px;
@@ -77,13 +73,21 @@
 		transition: background-color 0.3s ease-in;
 	}
 
+	#how {
+		font-size: var(--xl);
+		padding-bottom: 0px;
+		margin-bottom: 0px;
+	}
+
 	ol {
+		margin-top: 12px;
+		padding-top: 0px;
 		margin-bottom: 0px;
 	}
 
 	li {
 		font-size: var(--m);
-		margin-top: 12px;
+		margin-top: 16px;
 	}
 
 	.highlight {
