4019dd7bf84476f8de17870e63a51575f68b5b32
iliyangermanov <iliyan.germanov971@gmail.com>
2023-07-27 14:02:17 +0300

    Simplify the `HomeButton`

--- app/src/lib/navbar/HomeButton.svelte
+++ app/src/lib/navbar/HomeButton.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import LogoDark from '$lib/assets/hyperpeak-logo-dark.svg';
 	import LogoHovered from '$lib/assets/hyperpeak-logo-hovered.svg';
-	import Logo from '$lib/assets/hyperpeak-logo.svg';
+	import LogoLight from '$lib/assets/hyperpeak-logo-light.svg';
+	import { ds } from '$lib/design-system';
 
-	let src = Logo;
+	let src = $ds.isLight ? LogoLight : LogoDark;
 
 	function onHover() {
 		src = LogoHovered;
 	}
 
 	function onHoverEnd() {
-		src = Logo;
+		src = $ds.isLight ? LogoLight : LogoDark;
 	}
+
+	ds.subscribe((ds) => {
+		src = ds.isLight ? LogoLight : LogoDark;
+	});
 </script>
 
 {#if $page.url.pathname != '/'}
