4cebd0d117c6ca0f9f0fe79962b0eb8c171ec484
iliyangermanov <iliyan.germanov971@gmail.com>
2023-07-29 00:01:39 +0300

    Add `Project` component skeleton

---
+++ app/src/routes/projects/components/Project.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let logo: string;
+    export let name: string;
+    export let tags: [string];
+    export let description: string;
+    export let href: string;
+</script>
+
+<div id="project">
+    
+</div>
+
+<style>
+    #project {
+
+    }
+</style>
