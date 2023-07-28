234a7ed4d585e148213636da96781ae78a1037fd
iliyangermanov <iliyan.germanov971@gmail.com>
2023-07-28 14:02:01 +0300

    Add `Technology` component

--- app/src/routes/services/+page.svelte
+++ app/src/routes/services/+page.svelte
@@ -1,3 +1,8 @@
-
+<script lang="ts">
+	import Kotlin from '$lib/assets/technologies/kotlin-logo.svg';
+	import Technology from './components/Technology.svelte';
+</script>
 
 <h1>Services</h1>
+
+<Technology name="Kotlin" logo={Kotlin} url="https://kotlinlang.org/" />
---
+++ app/src/routes/services/components/Technology.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	export let name: string;
+	export let logo: string;
+	export let logoDark: string | null = null;
+	export let url: string;
+
+	import { ds } from '$lib/design-system';
+
+	let logoSrc = logo;
+
+	if (logoDark != null) {
+		ds.subscribe((ds) => {
+			logoSrc = ds.isLight ? logo : logoDark!;
+		});
+	}
+</script>
+
+<a href={url}>
+	<img src={logoSrc} alt={name} />
+	<span>{name}</span>
+</a>
+
+<style>
+	a {
+		display: flex;
+		flex-direction: row;
+		text-decoration: none;
+		align-items: center;
+		gap: 8px;
+		font-size: var(--m);
+	}
+
+	a:hover {
+		color: var(--secondary);
+	}
+
+	img {
+		width: 24px;
+		height: 24px;
+	}
+</style>
